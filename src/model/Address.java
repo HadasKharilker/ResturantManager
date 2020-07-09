f7b849de864be7820e8f0024f93ddeb4f9d930e7
yardengabay <65913909+yardengabay@users.noreply.github.com>
2020-07-09 20:25:44 +0300

    address

--- src/model/Address.java
+++ src/model/Address.java
@@ -16,6 +16,38 @@ public class Address implements Serializable {
         this.state = addressBuilder.state;
     }
 
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
     @Override
     public String toString() {
         return "[House number=" + houseNumber + ", Street=" + street + ", City=" + city + "]";
